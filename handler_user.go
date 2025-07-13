75cd45eb40a5dee6f25f3fb632c0c698d5b65ff1
dr-check <dustin.r.check@gmail.com>
2025-07-13 19:40:58 -0400

    A: Set up initial server logic and database.

---
+++ handler_user.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func (c *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type returnVals struct {
+		Id        string `json:"id"`
+		CreatedAt string `json:"created_at"`
+		UpdatedAt string `json:"updated_at"`
+		Email     string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	user, err := c.database.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, returnVals{
+		Id:        user.ID.String(),
+		CreatedAt: user.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: user.UpdatedAt.Format(time.RFC3339),
+		Email:     user.Email,
+	})
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+	"sync/atomic"
+
+	"github.com/dr-check/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	database       *database.Queries
+	env            string
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	godotenv.Load(".env")
+
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+
+	dbQueries := database.New(db)
+
+	sMux := http.NewServeMux()
+
+	apiCfig := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		database:       dbQueries,
+		env:            os.Getenv("PLATFORM"),
+	}
+
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+
+	sMux.Handle("/app/", apiCfig.middlewareMetricsInc(handler))
+
+	sMux.HandleFunc("POST /api/users", apiCfig.handlerCreateUser)
+
+	sMux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
+	sMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200 OK\n"))
+	})
+
+	sMux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		count := apiCfig.fileserverHits.Load()
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`
+	<html>
+
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+
+	</html>
+		`, count)))
+	})
+
+	sMux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfig.fileserverHits = atomic.Int32{}
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200 OK\n"))
+
+		if apiCfig.env == "dev" {
+			err := dbQueries.DeleteUsers(r.Context())
+			if err != nil {
+				http.Error(w, "Failed to delete users", http.StatusInternalServerError)
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+		}
+	})
+
+	newServer := &http.Server{
+		Addr:    ":8080",
+		Handler: sMux,
+	}
+
+	err2 := newServer.ListenAndServe()
+	if err2 != nil {
+		panic(err2)
+	}
+}
