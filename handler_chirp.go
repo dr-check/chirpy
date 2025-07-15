d04bca68af3024eb7197bf6837b721a5f77d6de0
dr-check <dustin.r.check@gmail.com>
2025-07-15 01:43:08 -0400

    B: Added logic to submit a chirp to the database.

---
+++ handler_chirp.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dr-check/chirpy/internal/database"
+	"github.com/google/uuid"
+)
+
+func (c *apiConfig) handlerChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+	type returnVals struct {
+		Id        string `json:"id"`
+		CreatedAt string `json:"created_at"`
+		UpdatedAt string `json:"updated_at"`
+		Body      string `json:"body"`
+		UserID    string `json:"user_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp body exceeds maximum length", nil)
+		return
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	cleaned := getCleanedBody(params.Body, badWords)
+
+	chirp, err := c.database.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleaned,
+		UserID: params.UserID,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, returnVals{
+		Id:        chirp.ID.String(),
+		Body:      chirp.Body,
+		CreatedAt: chirp.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: chirp.UpdatedAt.Format(time.RFC3339),
+		UserID:    chirp.UserID.String(),
+	})
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 	sMux.HandleFunc("POST /api/users", apiCfig.handlerCreateUser)
 
-	sMux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+	sMux.HandleFunc("POST /api/chirps", apiCfig.handlerChirp)
 
 	sMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
