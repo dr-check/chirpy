3567bee131fb4309dd14cabaabc20346338c0943
dr-check <dustin.r.check@gmail.com>
2025-07-20 00:33:33 -0400

    F: Added authentication functions for login.

---
+++ internal/auth/jwt.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	parsedToken, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	userID, ok := parsedToken.Claims.(*jwt.RegisteredClaims)
+	if !ok || !parsedToken.Valid {
+		return uuid.Nil, jwt.ErrSignatureInvalid
+	}
+	id, err := uuid.Parse(userID.Subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return uuid.UUID(id), nil
+}
